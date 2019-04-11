6e46529d944ac59e0a6d75cc2d1b3f4829f8aa3a
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2019-04-10 19:35:57 -0700

    update route to get sample businesses list

--- src/server/routeHandlers.js
+++ src/server/routeHandlers.js
@@ -1,9 +1,34 @@
 const db = require('./db')
 
 const index = (req, res) => {
-    res.sendFile("it works")
+    res.send("it works")
+}
+
+const getBusinesses = (req, res) => {
+    let businesses = [{
+        'name' : "Name A",
+        'business_id': "ID1"
+    },
+    {
+        'name' : "Name B",
+        'business_id': "ID2"
+    },
+    {
+        'name' : "Name C",
+        'business_id': "ID3"
+    },
+    {
+        'name' : "Name D",
+        'business_id': "ID4"
+    },
+    {
+        'name' : "Name E",
+        'business_id': "ID5"
+    }]
+    res.json(businesses)
 }
 
 module.exports = {
-    index
+    index,
+    getBusinesses
 }
--- src/server/routes.js
+++ src/server/routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const handlers = require('./routeHandlers')
 
 router.get('/', handlers.index)
+router.get('/businesses/', handlers.getBusinesses)
 
 module.exports = router
