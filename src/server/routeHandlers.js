b4d037d6fba48cab1d5113602165b3cc855dec06
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2019-04-10 20:25:18 -0700

    add sample data for loc search

--- src/server/routeHandlers.js
+++ src/server/routeHandlers.js
@@ -4,8 +4,8 @@ const index = (req, res) => {
     res.send("it works")
 }
 
-const getBusinesses = (req, res) => {
-    let businesses = [{
+const getBusinessNames = (req, res) => {
+    let data = [{
         'name' : "Name A",
         'business_id': "ID1"
     },
@@ -25,10 +25,45 @@ const getBusinesses = (req, res) => {
         'name' : "Name E",
         'business_id': "ID5"
     }]
-    res.json(businesses)
+    res.json(data)
+}
+
+const getBusinessesInArea = (req, res) => {
+    let data = [{
+        'name' : "Name A",
+        'business_id': "ID1",
+        'lat': req.params.minLat,
+        'long': req.params.minLong
+    },
+    {
+        'name' : "Name B",
+        'business_id': "ID2",
+        'lat': req.params.minLat,
+        'long': req.params.minLong
+    },
+    {
+        'name' : "Name C",
+        'business_id': "ID3",
+        'lat': req.params.minLat,
+        'long': req.params.minLong
+    },
+    {
+        'name' : "Name D",
+        'business_id': "ID4",
+        'lat': req.params.minLat,
+        'long': req.params.minLong
+    },
+    {
+        'name' : "Name E",
+        'business_id': "ID5",
+        'lat': req.params.minLat,
+        'long': req.params.minLong
+    }]
+    res.json(data)
 }
 
 module.exports = {
     index,
-    getBusinesses
+    getBusinessNames,
+    getBusinessesInArea
 }
--- src/server/routes.js
+++ src/server/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const handlers = require('./routeHandlers')
 
 router.get('/', handlers.index)
-router.get('/businesses/', handlers.getBusinesses)
+router.get('/businesses/', handlers.getBusinessNames)
+router.get('/businesses/:minLat/:maxLat/:minLong/:maxLong', handlers.getBusinessesInArea)
 
 module.exports = router
