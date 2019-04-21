6ef49f875da608236e40627b0db1a8adcc2da2ca
Ayush Ray <i.ayushray95@gmail.com>
2019-04-20 20:04:48 -0700

    initial search form tested

---
+++ src/client/js/search_submit.js
@@ -0,0 +1,8 @@
+function search()
+{
+	console.log("yo boi")
+	var business = document.getElementById("business_autocomplete").value;
+	var location = document.getElementById("autocomplete").value;
+	console.log(business)
+	console.log(location)
+}
