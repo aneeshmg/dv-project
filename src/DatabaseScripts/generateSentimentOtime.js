f1b46aa29800cce3c564304fc420164170c0932a
aneeshmg <aneesh.mg2007@gmail.com>
2019-04-22 22:07:30 -0700

    sentiment o time done

--- src/DatabaseScripts/generateSentimentOtime.js
+++ src/DatabaseScripts/generateSentimentOtime.js
@@ -1,13 +1,25 @@
 const fastCsv = require('fast-csv')
 const fs = require('fs')
 const createCsvWriter = require('csv-writer').createObjectCsvWriter
+const MongoClient = require('mongodb').MongoClient
 
-const fullFile = '../../data/processed.csv'
+const fullFile = '../../data/reviews_to_db.csv'
 const fullFileOut = '../../data/SentimentOverTime.csv'
 
 let business_ids = new Set()
 let years = new Set()
 let i = 0
+let rows = []
+
+const csvWriter = createCsvWriter({
+    path: fullFileOut,
+    header: [{
+        business_id: 'business_id',
+        year : 'year',
+        score : 'score'
+    }]
+})
+
 
 const istream = fs.createReadStream(fullFile)
 
@@ -21,23 +33,59 @@ let csvStream = fastCsv().on('data', data => {
 
 
 }).on('end', () => {
-    business_ids = getRandomSubarray([...business_ids], 100)
+    business_ids = getRandomSubarray([...business_ids], 10)
     years = [...years]
-    console.log(business_ids.length)
-    console.log(years)
+    // console.log(business_ids.length)
+    // console.log(years)
+    let db
 
-    let numberOfBusinesses = business_ids.length
-    let numberOfYears = years.length
+    MongoClient.connect('mongodb://localhost:27017', (err, _db) => {
+        if (err) throw err
+        db = _db.db('dv')
 
-    for (let i = 0; i < numberOfBusinesses; i++) {
+        console.log('Connected to db..')
 
-        for (let j = 0; j < numberOfYears; j++) {
+        let numberOfBusinesses = business_ids.length
+        let numberOfYears = years.length
 
-            let sentimentScore = 0
-            let review = getFromDb()
+        for (let i = 0; i < numberOfBusinesses; i++) {
+
+            for (let j = 0; j < numberOfYears; j++) {
+
+                db.collection('reviews_to_db').find({
+                    business_id: business_ids[i],
+                    "$expr": {
+                        "$eq": [{
+                            "$year": "$date"
+                        }, parseInt(years[j])]
+                    }
+                }).toArray((err, data) => {
+
+                    let n = data.length
+                    let score = 0
+                    for (let k = 0; k < n; k++) {
+                        score += parseFloat(data[k].score)
+                    }
+                    
+                    rows.push({
+                        business_id : business_ids[i],
+                        year : years[j],
+                        score : score
+                    })
+
+                })
+            }
+            console.log(rows.length)
+
         }
+        console.log('Finally rowcount', rows.length)
 
-    }
+        csvWriter.writeRecords(rows).then(() => {
+            console.log(`Wrote to file..${++i}`)
+        })
+
+        _db.close()
+    })
 
 
     console.log('END')
@@ -47,7 +95,9 @@ istream.pipe(csvStream)
 
 
 const getRandomSubarray = (arr, size) => {
-    var shuffled = arr.slice(0), i = arr.length, temp, index;
+    var shuffled = arr.slice(0),
+        i = arr.length,
+        temp, index;
     while (i--) {
         index = Math.floor((i + 1) * Math.random());
         temp = shuffled[index];
--- temp.js
+++ temp.js
@@ -1,5 +1,30 @@
-const db = require('./src/server/db')
+// const db = require('./src/server/db')
 
-db.connectToDatabase().then(() => {
-    const dbCon = db.getDb()
+// db.connectToDatabase().then(() => {
+//     const dbCon = db.getDb()
+// })
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://localhost:27017', (err, _db) => {
+    if (err) throw err
+    db = _db.db('dv')
+    console.log("Connected successfully to db...")
+
+
+    // db.collection('reviews_to_db').find({date: '/.*/'}).toArray((er, data) => {
+    //     console.log(data)
+    //     _db.close()
+    // })
+
+    // Requires official MongoShell 3.6+
+
+    db.collection('reviews_to_db').find({ 
+        business_id: "zvO-PJCpNk4fgAVUnExYAA", 
+        "$expr": { "$eq": [{ "$year": "$date" }, 2010] } 
+    }).toArray((err, data) => {
+        console.log(data.reduce((ac, cu) => ac += parseFloat(cu.score), 0))
+    })
+
+    _db.close()
+
 })
