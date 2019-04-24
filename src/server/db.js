aff9845739a016c968bfb2b458c8fb4c122dadab
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2019-04-24 00:32:12 -0700

    cleanup code

--- src/server/db.js
+++ src/server/db.js
@@ -1,55 +1,33 @@
-//const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient
 const config = require('./config')
-const logger = require('./logger')
+const log = require('./logger')
 
-// let _db
-
-// module.exports = {
-//     // TODO: verify this properly - promise, error handling, singleton, logging *?
-//     connectToDatabase: () => {
-//         return new Promise((res, rej) => {
-//             MongoClient.connect(`mongodb://localhost/${config.dbName}`).then(db => {
-//                 logger.info('Connected to db.')
-//                 _db = db
-//                 res();
-//             }).catch(err =>{
-//                 logger.error('Failed to connect to database: ', err)
-//             })
-//         })
-//       },
-//     getDb: () => {
-//         console.log('here came !!!!!!!!' +_db);
-//         return _db;
-//     }
-
-// }
-
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
-
-let _db;
+let _db
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    `mongodb://localhost/${config.dbName}`
-  )
+      `mongodb://localhost/${config.dbName}`
+    )
     .then(client => {
-      console.log('Connected!');
-      _db = client.db();
-      callback();
+      log.info('Connected!')
+      _db = client.db()
+      callback()
     })
     .catch(err => {
-      console.log(err);
-      throw err;
-    });
+      log.error(err)
+      throw err
+    })
 };
 
 const getDb = () => {
   if (_db) {
-    return _db;
+    return _db
   }
-  throw 'No database found!';
-};
+  throw 'No database found!'
+}
 
-exports.mongoConnect = mongoConnect;
-exports.getDb = getDb;
+
+module.exports = {
+  mongoConnect,
+  getDb
+}
--- src/server/routeHandlers.js
+++ src/server/routeHandlers.js
@@ -1,100 +1,94 @@
-const db = require('./db')
-const Business = require('./models/Business.js');
-const MongoClient = require('mongodb').MongoClient
+const Business = require('./models/Business.js')
+const dbPool = require('./db')
+const log = require('./logger')
 
 const index = (req, res) => {
     res.send("it works")
 }
 
 const getBusinessInfo = (req, res) => {
-    //console.log('HERE REACHED');
     const bussid = req.params.BussID;
-    //console.log(bussid);
     Business.findById(bussid)
-    // Product.findById(prodId)
-    .then(busobj => {
-      if (!busobj) {
-        return res.redirect('/');
-      }
-      res.json(busobj);
-    })
-    .catch(err => console.log(err));
+        .then(busobj => {
+            if (!busobj) {
+                return res.redirect('/');
+            }
+            res.json(busobj);
+        })
+        .catch(err => log.error(err));
 }
 
 const getAllBusinesses = (req, res) => {
-    //console.log('HERE REACHED');
-    //const bussid = req.params.BussID;
-    //console.log(bussid);
     Business.fetchAll()
-    // Product.findById(prodId)
-    .then(busobj => {
-      if (!busobj) {
-        return res.redirect('/');
-      }
-      res.json(busobj);
-    })
-    .catch(err => console.log(err));
+        .then(busobj => {
+            if (!busobj) {
+                return res.redirect('/');
+            }
+            res.json(busobj);
+        })
+        .catch(err => log.error(err));
 }
 
 const getBusinessesbycoord = (req, res) => {
-    //console.log('LOCA CHECK REACHED');
     const lat1 = req.params.lat1;
     const long1 = req.params.long1;
     const lat2 = req.params.lat2;
     const long2 = req.params.long2;
-    console.log(lat2);
-    Business.findWithinLoc(lat1,long1,lat2,long2)
-    // Product.findById(prodId)
-    .then(busobj => {
-      if (!busobj) {
-        return res.redirect('/');
-      }
-      res.json(busobj);
-    })
-    .catch(err => console.log(err));
+    log.info(lat2);
+    Business.findWithinLoc(lat1, long1, lat2, long2)
+        .then(busobj => {
+            if (!busobj) {
+                return res.redirect('/');
+            }
+            res.json(busobj);
+        })
+        .catch(err => log.error(err));
 }
 
 const getBusinessesbyName = (req, res) => {
-    //console.log('LOCA CHECK REACHED');
     const Bizname = req.params.Bizname;
     Business.findByBuzName(Bizname)
-    // Product.findById(prodId)
-    .then(busobj => {
-      if (!busobj) {
-        return res.redirect('/');
-      }
-      res.json(busobj);
-    })
-    .catch(err => console.log(err));
+        .then(busobj => {
+            if (!busobj) {
+                return res.redirect('/');
+            }
+            res.json(busobj);
+        })
+        .catch(err => log.error(err));
 }
 
 const getSentiments = (req, res) => {
-    MongoClient.connect('mongodb://localhost:27017', (err, _db) => {
-        if (err) throw err
-        db = _db.db('dv')
+    const db = dbPool.getDb()
 
-        db.collection('reviews_to_db').find({ 
-            business_id: req.params.business_id, 
-            date: parseInt(req.params.year) 
-        }).toArray((err, data) => {
-            if (data == null) res.json({})
-            else {
-                let score = data.reduce((ac, cu) => ac += parseFloat(cu.score), 0)
-                res.json({
-                    business_id : req.params.business_id,
-                    year: req.params.year,
-                    sentiment_score: score
-                })
-            }
-        })
-        _db.close()
+    db.collection('reviews_to_db').find({
+        business_id: req.params.business_id,
+        date: parseInt(req.params.year)
+    }).toArray((err, data) => {
+        if (err) {
+            log.error(err)
+            throw err
+        }
+        if (data == null) res.json({})
+        else {
+            let score = data.reduce((ac, cu) => ac += parseFloat(cu.score), 0)
+            res.json({
+                business_id: req.params.business_id,
+                year: req.params.year,
+                sentiment_score: score
+            })
+        }
     })
 }
 
+const getKeyTerms = (req, res) => {
+    const db = dbPool.getDb()
+
+
+}
+
+
 module.exports = {
     index,
-    getBusinessNames,
-    getBusinessesInArea,
     getBusinessInfo,
     getBusinessesbycoord,
     getBusinessesbyName,
