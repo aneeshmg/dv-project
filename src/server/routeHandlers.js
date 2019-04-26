3e09139ea2dcca845524892295d02acc8670760c
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2019-04-25 21:32:45 -0700

    fix merge conflicts

--- src/server/routeHandlers.js
+++ src/server/routeHandlers.js
@@ -3,72 +3,72 @@ const dbPool = require("./db");
 const log = require("./logger");
 
 const index = (req, res) => {
-  res.send("it works");
+    res.send("it works");
 };
 
 const getBusinessInfo = (req, res) => {
-  const bussid = req.params.BussID;
-  Business.findById(bussid)
-    .then(busobj => {
-      if (!busobj) {
-        return res.redirect("/");
-      }
-      res.json(busobj);
-    })
-    .catch(err => log.error(err));
+    const bussid = req.params.BussID;
+    Business.findById(bussid)
+        .then(busobj => {
+            if (!busobj) {
+                return res.redirect("/");
+            }
+            res.json(busobj);
+        })
+        .catch(err => log.error(err));
 };
 
 const getAllBusinesses = (req, res) => {
-  Business.fetchAll()
-    .then(busobj => {
-      if (!busobj) {
-        return res.redirect("/");
-      }
-      res.json(busobj);
-    })
-    .catch(err => log.error(err));
+    Business.fetchAll()
+        .then(busobj => {
+            if (!busobj) {
+                return res.redirect("/");
+            }
+            res.json(busobj);
+        })
+        .catch(err => log.error(err));
 };
 
 const getBusinessesbyNameCityState = (req, res) => {
-  const Bizname = req.params.Bizname;
-  const Bizcity = req.params.Bizcity;
-  const Bizstate = req.params.Bizstate;
-  Business.findByBiznamecitystate(Bizname, Bizcity, Bizstate)
-    .then(busobj => {
-      if (!busobj) {
-        return res.redirect("/");
-      }
-      res.json(busobj);
-    })
-    .catch(err => log.error(err));
+    const Bizname = req.params.Bizname;
+    const Bizcity = req.params.Bizcity;
+    const Bizstate = req.params.Bizstate;
+    Business.findByBiznamecitystate(Bizname, Bizcity, Bizstate)
+        .then(busobj => {
+            if (!busobj) {
+                return res.redirect("/");
+            }
+            res.json(busobj);
+        })
+        .catch(err => log.error(err));
 };
 
 const getBusinessesbycoord = (req, res) => {
-  const lat1 = req.params.lat1;
-  const long1 = req.params.long1;
-  const lat2 = req.params.lat2;
-  const long2 = req.params.long2;
-  log.info(lat2);
-  Business.findWithinLoc(lat1, long1, lat2, long2)
-    .then(busobj => {
-      if (!busobj) {
-        return res.redirect("/");
-      }
-      res.json(busobj);
-    })
-    .catch(err => log.error(err));
+    const lat1 = req.params.lat1;
+    const long1 = req.params.long1;
+    const lat2 = req.params.lat2;
+    const long2 = req.params.long2;
+    log.info(lat2);
+    Business.findWithinLoc(lat1, long1, lat2, long2)
+        .then(busobj => {
+            if (!busobj) {
+                return res.redirect("/");
+            }
+            res.json(busobj);
+        })
+        .catch(err => log.error(err));
 };
 
 const getBusinessesbyName = (req, res) => {
-  const Bizname = req.params.Bizname;
-  Business.findByBuzName(Bizname)
-    .then(busobj => {
-      if (!busobj) {
-        return res.redirect("/");
-      }
-      res.json(busobj);
-    })
-    .catch(err => log.error(err));
+    const Bizname = req.params.Bizname;
+    Business.findByBuzName(Bizname)
+        .then(busobj => {
+            if (!busobj) {
+                return res.redirect("/");
+            }
+            res.json(busobj);
+        })
+        .catch(err => log.error(err));
 };
 
 const getBusinessesbyNameCityState = (req, res) => {
@@ -86,45 +86,37 @@ const getBusinessesbyNameCityState = (req, res) => {
 }
 
 const getSentiments = (req, res) => {
-  const db = dbPool.getDb();
+    const db = dbPool.getDb();
 
-  db.collection("main")
-    .find(
-      {
-        business_id: req.params.business_id,
-        date: req.params.year
-<<<<<<< Updated upstream
-    }, {
-=======
-      },
-      {
->>>>>>> Stashed changes
-        business_name: 1,
-        sentiment_score: 1
-      }
-    )
-    .toArray((err, data) => {
-      if (err) {
-        log.error(err);
-        throw err;
-      }
-      if (data == null || data.length == 0) res.json({});
-      else {
-        let score = data.reduce(
-          (ac, cu) => (ac += parseFloat(cu.sentiment_score)),
-          0
-        );
-        res.json({
-          business_id: req.params.business_id,
-          year: req.params.year,
-          business_name: data[0].business_name,
-          sentiment_score: score
+    db.collection("main")
+        .find({
+            business_id: req.params.business_id,
+            date: req.params.year
+        }, {
+            business_name: 1,
+            sentiment_score: 1
+        })
+        .toArray((err, data) => {
+            if (err) {
+                log.error(err);
+                throw err;
+            }
+            if (data == null || data.length == 0) res.json({});
+            else {
+                let score = data.reduce(
+                    (ac, cu) => (ac += parseFloat(cu.sentiment_score)),
+                    0
+                );
+                res.json({
+                    business_id: req.params.business_id,
+                    year: req.params.year,
+                    business_name: data[0].business_name,
+                    sentiment_score: score
+                });
+            }
         });
-      }
-    });
 };
 
-<<<<<<< Updated upstream
 const getPositiveTerms = (req, res) => {
     const db = dbPool.getDb()
 
@@ -180,45 +172,10 @@ const getNegativeTerms = (req, res) => {
         }
     })
 }
-=======
-const getKeyTerms = (req, res) => {
-  const db = dbPool.getDb();
-
-  db.collection("business_with_positives")
-    .find(
-      {
-        business_id: req.params.business_id
-      },
-      {
-        positives: 1,
-        name: 1,
-        negatives: 1
-      }
-    )
-    .toArray((err, data) => {
-      if (err) {
-        log.error(err);
-        throw err;
-      }
-      if (data == null || data.length == 0) res.json({});
-      else {
-        let positives = data[0].positives.split(" ");
-        let negatives = data[0].negatives.split(" ");
-        res.json({
-          business_id: req.params.business_id,
-          business_name: data[0].name,
-          positives: positives,
-          negatives: negatives
-        });
-      }
-    });
-};
->>>>>>> Stashed changes
 
 const getRatings = (req, res) => {
-  const db = dbPool.getDb();
+    const db = dbPool.getDb();
 
-<<<<<<< Updated upstream
     // TODO: fix rating once db is ready
     db.collection('main').find({
         business_id: req.params.business_id,
@@ -310,45 +267,3 @@ module.exports = {
     getRatings,
     getTopics
 }
-=======
-  // TODO: fix rating once db is ready
-  db.collection("main")
-    .find({
-      business_id: req.params.business_id,
-      date: req.params.year
-    })
-    .toArray((err, data) => {
-      if (err) {
-        log.error(err);
-        throw err;
-      }
-      if (data == null || data.length == 0) res.json({});
-      else {
-        let o = data.map(e => {
-          return {
-            business_id: req.params.business_id,
-            reviewer_id: e.user_id,
-            business_name: e.business_name,
-            impact_score: e.impact_score,
-            year: e.date,
-            rating: Math.floor(Math.random() * 5) + 1
-          };
-        });
-        console.log(o);
-        res.send(o);
-      }
-    });
-};
-
-module.exports = {
-  index,
-  getBusinessInfo,
-  getBusinessesbycoord,
-  getBusinessesbyName,
-  getBusinessesbyNameCityState,
-  getAllBusinesses,
-  getSentiments,
-  getKeyTerms,
-  getRatings
-};
->>>>>>> Stashed changes
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,43 +1,20 @@
 const router = require("express").Router();
 const handlers = require("./routeHandlers");
 
-<<<<<<< Updated upstream
 router.get('/', handlers.index)
 router.get('/getbusinessbyID/:BussID',handlers.getBusinessInfo)
 router.get('/getBusinessesWithinLoc/:lat1/:long1/:lat2/:long2',handlers.getBusinessesbycoord)
 router.get('/getbusinessbyname/:Bizname',handlers.getBusinessesbyName)
 router.get('/getbusinesses/',handlers.getAllBusinesses)
 router.get('/getbusinessesbynamecitystate/:Bizname/:Bizcity/:Bizstate',handlers.getBusinessesbyNameCityState)
-=======
-router.get("/", handlers.index);
-router.get("/getbusinessbyID/:BussID", handlers.getBusinessInfo);
-router.get(
-  "/getBusinessesWithinLoc/:lat1/:long1/:lat2/:long2",
-  handlers.getBusinessesbycoord
-);
-router.get("/getbusinessbyname/:Bizname", handlers.getBusinessesbyName);
-router.get("/getbusinesses/", handlers.getAllBusinesses);
-router.get(
-  "/getbusinessesbynamecitystate/:Bizname/:Bizcity/:Bizstate",
-  handlers.getBusinessesbyNameCityState
-);
->>>>>>> Stashed changes
 
 router.get("/sentiment/:business_id/:year", handlers.getSentiments);
 
-<<<<<<< Updated upstream
 router.get('/positives/:business_id', handlers.getPositiveTerms)
 router.get('/negatives/:business_id', handlers.getNegativeTerms)
-=======
-router.get("/positivesNegatives/:business_id", handlers.getKeyTerms);
->>>>>>> Stashed changes
 
 router.get("/ratings/:business_id/:year", handlers.getRatings);
 
-<<<<<<< Updated upstream
 router.get('/topics/:business_id/:year', handlers.getTopics)
 
 module.exports = router
-=======
-module.exports = router;
->>>>>>> Stashed changes
